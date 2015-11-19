93afd1cdcb238cef36987ac9ac421b453f17a0b5
Casey Law <caseyjlaw@gmail.com>
2015-11-19 08:03:34 -0700

    upping version number to synchronize all

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='sdmreader',
-      version='1.1',
+      version='1.2',
       description='Read/parse NRAO Science Data Model files',
       author='Casey Law',
       author_email='caseyjlaw@gmail.com',
